5e31a003436128ad337834691230d667ed43ca27
DzmitryBasiachenka <bsdim333@gmail.com>
2016-11-18 09:52:11 +0300

    Change MainMenu

--- tlj-admin/src/main/java/com/bsdim/tlj/admin/menu/MainMenu.java
+++ tlj-admin/src/main/java/com/bsdim/tlj/admin/menu/MainMenu.java
@@ -4,9 +4,10 @@ import com.bsdim.tlj.admin.AbstractMenu;
 import com.bsdim.tlj.admin.service.UserFacade;
 
 public class MainMenu {
+    UserFacade objectUserFacade = new UserFacade();
+
     public String selectMenu() {
         AbstractMenu userMenu = new UserMenu();
-        UserFacade objectUserFacade = new UserFacade();
         while (true) {
             int  number = userMenu.performMenu();
             switch (number) {
