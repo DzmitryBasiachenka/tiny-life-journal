b4e3fcf24ab3dd461c9aa87f914d0cfa6184c14d
DzmitryBasiachenka <bsdim333@gmail.com>
2016-12-06 14:08:35 +0300

    Change class FileRepository

--- tlj-admin/src/main/java/com/bsdim/tlj/admin/FileRepository.java
+++ tlj-admin/src/main/java/com/bsdim/tlj/admin/FileRepository.java
@@ -7,13 +7,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public abstract class FileRepository {
+public abstract class FileRepository<T> {
     private String file;
     public FileRepository(String newFile){
         this.file = newFile;
     }
 
-    public void writeData(List<User> users) {
+    public void writeData(List<T> users) {
         try (ObjectOutputStream write = new ObjectOutputStream(new FileOutputStream(file))) {
             write.writeObject(users);
             System.out.println("File save");
@@ -22,10 +22,10 @@ public abstract class FileRepository {
         }
     }
 
-    public List<User> readData(){
+    public List<T> readData(){
         if ((new File(file).exists())) {
             try (ObjectInputStream read = new ObjectInputStream(new FileInputStream(file))) {
-                return (List<User>) read.readObject();
+                return (List<T>) read.readObject();
             } catch (Exception ex) {
                 System.out.println(ex.getMessage());
             }
