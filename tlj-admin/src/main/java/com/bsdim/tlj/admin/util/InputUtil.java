ccb2cd361bc8bd6c609bc33fe383e19515ef6977
DzmitryBasiachenka <bsdim333@gmail.com>
2016-12-20 16:05:06 +0300

    Add method createDataUser

--- tlj-admin/src/main/java/com/bsdim/tlj/admin/util/InputUtil.java
+++ tlj-admin/src/main/java/com/bsdim/tlj/admin/util/InputUtil.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class InputUtil {
     private static Scanner scanner = new Scanner(System.in);
 
+
     public static Scanner getScanner() {
         return scanner;
     }
