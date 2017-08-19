576174ba24406502109161a6bb01c3ea0478f372
DzmitryBasiachenka <bsdim333@gmail.com>
2017-08-19 19:40:09 +0300

    Added check of user data in the filter

---
+++ tlj-ui-web/src/main/java/com/bsdim/tlj/ui/web/session/UserSession.java
@@ -0,0 +1,31 @@
+package com.bsdim.tlj.ui.web.session;
+
+public class UserSession {
+    private String id;
+    private String login;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
